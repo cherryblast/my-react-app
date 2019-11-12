7d76042880f1a07a20895360d2cd904e764ae81d
cherryblast <deps@DESKTOP-7FRK9R7.localdomain>
2019-11-12 19:11:54 +0900

    1112_퇴근전

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
@@ -6,61 +6,56 @@ import Movie from './Movie';
 
 class App extends Component {
 
- state = {
-
- }
-
+ state = {}
+ 
  componentDidMount(){
-   setTimeout(() => {
-     this.setState({
-       movies: [
-        {
-          title: "Matrix",
-          poster: "https://www.maxim.com/.image/t_share/MTU0OTMxNjA5NTU0OTIxMTUz/r-lee-ermey-promo.jpg"
-      
-        },
-        {
-          title: "Full Metal Jacket",
-          poster: "https://images.velog.io/post-images/killi8n/54206690-bf18-11e8-96f6-87ddba9fb88b/ReactDjango.jpg"
-        },
-        {
-          title: "Oldboy",
-          poster: "https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png"
-      
-        },
-        {
-          title: "star wars",
-          poster: "https://images.velog.io/post-images/vies00/b0872660-0141-11e9-a384-ad27235bab63/bzu1hz4tglecz9nj8gaj.webp"
-      
-        },
+    this._getMovies();
+  }
 
-        {
-          title: "Trainspotting",
-          poster: "https://images.velog.io/post-images/vies00/b0872660-0141-11e9-a384-ad27235bab63/bzu1hz4tglecz9nj8gaj.webp"
-        }
-         
-       ]
-   })
-  }, 5000)
- }
+ /* promise란 앞서 로딩중인 스택에서 에러가 발생할것을 대비해 상황을 염두에 둠으로써 문제가 생겨도 다음 스택으로 넘어가 
+ 로딩을 시작한다. 사용방법은 fetch('json') .then(situration => situration.json()) .then(json => console.log(json)) .catch(err => console.log(err))
+ 로딩이 성공적으로 수행 = pass 실패시 결과를 catch, then으로 받아볼수있음. 무한정 에러를 catch로 잡고, fetch로 url을 ajax로 불러올수있다. */
+
+ 
 
  _renderMovies = () => {
-   const movies = this.state.movies.map((movie, index) => {
-     return <Movie title={movie.title} poster={movie.poster} key={index} />
+   const movies = this.state.movies.map((movie) => {
+     console.log(movie)
+     return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
    })
    return movies
+ }
+
+ _getMovies = async () => {
+  const movies = await this._callApi()
+  this.setState({
+    movies 
+  })
+ }
+
+ /* await 와 async는 같이 사용 */
+
+ _callApi = () => {
+  return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+  .then(potato => potato.json())
+  .then(json => json.data.movies)
+  .catch(err => console.log(err))
  }
 
   render() {
     return (
       <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}    //로딩스테이트 만들기
-            //내가 찾고있는 데이터가 있는지 체크하고 자바스크립트에게 물어본다.
-            //참이라면 영화정보를 출력하고, 거짓일경우 로딩창을 출력한다.
+        {this.state.movies ? this._renderMovies() : 'Loading'}    
+        
+        {/* 로딩스테이트 만들기
+            내가 찾고있는 데이터가 있는지 체크하고 자바스크립트에게 물어본다.
+            참이라면 영화정보를 출력하고, 거짓일경우 로딩창을 출력한다. */}
+      
       </div>
     );
   }
 }
+
 
 
 export default App;
