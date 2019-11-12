717cb63f8c7c7fc1f27d356e780bc49fe05e9b1b
cherryblast <deps@DESKTOP-7FRK9R7.localdomain>
2019-11-12 15:44:54 +0900

    nopp

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "star Wars"
-]
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://www.maxim.com/.image/t_share/MTU0OTMxNjA5NTU0OTIxMTUz/r-lee-ermey-promo.jpg"
 
-const movieImages = [
-"https://www.maxim.com/.image/t_share/MTU0OTMxNjA5NTU0OTIxMTUz/r-lee-ermey-promo.jpg",
-"https://images.velog.io/post-images/killi8n/54206690-bf18-11e8-96f6-87ddba9fb88b/ReactDjango.jpg",
-"https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png",
-"https://images.velog.io/post-images/vies00/b0872660-0141-11e9-a384-ad27235bab63/bzu1hz4tglecz9nj8gaj.webp"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://images.velog.io/post-images/killi8n/54206690-bf18-11e8-96f6-87ddba9fb88b/ReactDjango.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png"
+
+  },
+  {
+    title: "star wars",
+    poster: "https://images.velog.io/post-images/vies00/b0872660-0141-11e9-a384-ad27235bab63/bzu1hz4tglecz9nj8gaj.webp"
+
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-      <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-      <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-      <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
-    )
+    );
   }
 }
+
 
 export default App;
