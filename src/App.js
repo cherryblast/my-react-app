55e22386fa036b35838fb3bbd8c840c9f35c29c4
cherryblast <deps@DESKTOP-7FRK9R7.localdomain>
2019-11-13 11:38:08 +0900

    final-최종

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Movie from './Movie';
 
 class App extends Component {
 
- state = {}
+ state = {};
  
- componentDidMount(){
+ componentDidMount() {
     this._getMovies();
   }
 
@@ -19,43 +19,50 @@ class App extends Component {
  
 
  _renderMovies = () => {
-   const movies = this.state.movies.map((movie) => {
-     console.log(movie)
-     return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
-   })
-   return movies
- }
+   const movies = this.state.movies.map(movie => {
+     return (
+     <Movie 
+     title={movie.title_english} 
+     poster={movie.medium_cover_image} 
+     key={movie.id} 
+     genres={movie.genres} 
+     synopsis={movie.synopsis}
+     />
+     );
+   });
+   return movies;
+ };
 
  _getMovies = async () => {
-  const movies = await this._callApi()
+  const movies = await this._callApi();
   this.setState({
     movies 
-  })
- }
+  });
+ };
 
  /* await 와 async는 같이 사용 */
 
  _callApi = () => {
-  return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+  return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
   .then(potato => potato.json())
   .then(json => json.data.movies)
-  .catch(err => console.log(err))
- }
+  .catch(err => console.log(err));
+ };
 
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}    
-        
-        {/* 로딩스테이트 만들기
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}    
+        </div>
+
+        /* 로딩스테이트 만들기
             내가 찾고있는 데이터가 있는지 체크하고 자바스크립트에게 물어본다.
-            참이라면 영화정보를 출력하고, 거짓일경우 로딩창을 출력한다. */}
+            참이라면 영화정보를 출력하고, 거짓일경우 로딩창을 출력한다. */
       
-      </div>
+    
     );
   }
 }
-
-
 
 export default App;
